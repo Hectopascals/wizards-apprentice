d52f77a62e6cc681f990f8cf1e7e1ca1d496bb77
Henry2 <henry-hoang@hotmail.com>
2016-05-07 21:17:45 -0400

    tiled the grass and added keypress

--- tilemap.cc
+++ tilemap.cc
@@ -17,7 +17,7 @@ public:
         m_vertices.resize(width * height * 4);
 
         // populate the vertex array, with one quad per tile
-        for (unsigned int i = 0; i < width; ++i)
+        for (unsigned int i = 0; i < width; ++i) {
             for (unsigned int j = 0; j < height; ++j) {
                 // get the current tile number
                 int tileNumber = tiles[i + j * width];
@@ -41,14 +41,12 @@ public:
                 quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
                 quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
             }
-
+        }
         return true;
     }
 
 private:
-
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
-    {
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
         // apply the transform
         states.transform *= getTransform();
 
@@ -66,33 +64,61 @@ private:
 
 int main() {
     // create the window
-    sf::RenderWindow window(sf::VideoMode(540, 540), "Tilemap");
+    sf::RenderWindow window(sf::VideoMode(540, 540), "Wizard's Apprentice");
+    window.setPosition(sf::Vector2i(700, 200));
 
     // define the level with an array of tile indices
     const int level[] = {
-        0, 1, 2, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
-        1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
-        0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
-        0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
-        0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
-        2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
-        0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1 };
+        0, 1, 2, 3, 0, 0, 1, 1, 1,
+        1, 1, 2, 1, 3, 0, 1, 2, 3,
+        0, 0, 3, 2, 1, 2, 2, 1, 3,
+        0, 0, 2, 1, 1, 0, 0, 1, 1,
+        2, 2, 2, 2, 2, 1, 1, 3, 0,
+        0, 2, 1, 3, 3, 3, 0, 0, 1,
+        0, 1, 0, 0, 1, 2, 1, 3, 2,
+        2, 1, 1, 0, 0, 3, 3, 3, 0,
+        3, 2, 1, 0, 0, 1, 2, 1, 1 };
 
     // create the tilemap from the level definition
     TileMap map;
-    if (!map.load("colors.jpg", sf::Vector2u(50, 50), level, 9, 9))
+    if (!map.load("./Images/grass.png", sf::Vector2u(60, 60), level, 9, 9))
         return -1;
 
     // run the main loop
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         // handle events
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if(event.type == sf::Event::Closed)
+        while (window.pollEvent(event)) {
+            // Clicked closed window
+            if (event.type == sf::Event::Closed)
                 window.close();
+
+            // Just to check for width/height resized
+            if (event.type == sf::Event::Resized) {
+                std::cout << "new width: " << event.size.width << std::endl;
+                std::cout << "new height: " << event.size.height << std::endl;
+            }
+
+            // Key Press
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Escape) {
+                    std::cout << "ESC was pressed" << std::endl;
+                    window.close();
+                    // 1 if pressed with ESC, else 0
+                    //std::cout << "ctrl:" << event.key.control << std::endl;
+                } else if (event.key.code == sf::Keyboard::Left) {
+                    std::cout << "Left was pressed" << endl;
+                } else if (event.key.code == sf::Keyboard::Right) {
+                    std::cout << "Right was pressed" << endl;
+                } else if (event.key.code == sf::Keyboard::Up) {
+                    std::cout << "Up was pressed" << endl;
+                } else if (event.key.code == sf::Keyboard::Down) {
+                    std::cout << "Down was pressed" << endl;
+                } else if (event.key.code == sf::Keyboard::Q) {
+                    std::cout << "Q was pressed" << endl;
+                }
+            }
+
         }
 
         // draw the map
